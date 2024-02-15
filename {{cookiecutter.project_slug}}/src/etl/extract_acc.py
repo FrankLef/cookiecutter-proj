7ed1af3009e14dc2957b60df0ffda4c9bbd5a03a
FrankLef <flefebvre01@hotmail.com>
2024-02-14 19:49:28 -0500

    fix: extract MS Access

--- {{cookiecutter.project_slug}}/src/etl/extract_acc.py
+++ {{cookiecutter.project_slug}}/src/etl/extract_acc.py
@@ -4,20 +4,19 @@ from pathlib import Path
 
 import pandas as pd
 import sqlalchemy as sa
-from prefect import task
 
 
-def build_engine(path: str) -> sa.engine:
+def build_engine(path: Path) -> sa.engine:
     """Create SQLAlchemy engine for MS Access.
     Args:
-        path (str): Path to MS Access database.
+        path (Path): Path to MS Access database.
     Returns:
         sa.engine: SQLAlchemy engine for MS Access.
     Raises:
         FileNotFoundError: The MS Access file name is invalid.
     """
-    if Path(path).is_file() is False:
-        msg = "\n" + path + "\nis an invalid MS Access DB file name."
+    if not path.is_file():
+        msg = "\n" + str(path) + "\nis an invalid MS Access DB file name."
         raise FileNotFoundError(msg)
     db_driver = "{Microsoft Access Driver (*.mdb, *.accdb)}"
     conn_str = f"DRIVER={db_driver};" f"DBQ={path};" "Mode=Read;"
@@ -27,36 +26,26 @@ def build_engine(path: str) -> sa.engine:
     return acc_engine
 
 
-def extract_data(tables: tuple[str, ...], engine: sa.engine) -> dict:
+def extract_acc(db_path: Path, tbl: str) -> pd.DataFrame:
     """Extract data from MS Access.
+
     Args:
-        tables (set[str]): Names of tables.
-        engine (sa.engine): SQLAlchemy engine for MS Access.
+        db_path (Path): MS Access db path.
+        tbl (str): Name of table to extract.
+
     Returns:
-        dict: Datasets from MS Access.
+        pd.Dataframe: Dataframe of extracted data.
     """
-
-    out = {}
-    for tbl in tables:
-        sql_str = "select * from " + tbl
-        try:
-            df = pd.read_sql(sql=sql_str, con=engine)
-        except sa.SQLAlchemyError as err:
-            # https://stackoverflow.com/questions/2136739/error-handling-in-sqlalchemy
-            # msg = str(err.__dict__["orig"])
-            msg = str(err)
-            msg = "THE ERROR:" + str(type(err))
-            print(msg)
-            raise
-        out[tbl] = df
+    engine = build_engine(db_path)
+    sql_str = "select * from " + tbl
+    try:
+        df = pd.read_sql(sql=sql_str, con=engine)
+    except sa.SQLAlchemyError as err:
+        # https://stackoverflow.com/questions/2136739/error-handling-in-sqlalchemy
+        # msg = str(err.__dict__["orig"])
+        msg = str(err)
+        msg = "THE ERROR:" + str(type(err))
+        print(msg)
+        raise
     engine.dispose()
-    return out
-
-
-@task
-def extract_acc(path: str, tables: tuple[str, ...]) -> dict:
-    acc_engine = build_engine(path=path)
-    data = extract_data(tables=tables, engine=acc_engine)
-    # print the shape of every table
-    # {tbl: print(df.shape) for (tbl, df) in raw_data.items()}
-    return data
+    return df
