fc85dc0b32167bc606c2e7472117965e97788519
FrankLef <flefebvre01@hotmail.com>
2024-12-22 14:52:47 -0500

    fix(file_namer)

--- {{cookiecutter.__project_slug}}/src/s0_helpers/file_namer.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/file_namer.py
@@ -1,7 +1,12 @@
 class FileNamer:
-    def fname(self, name: str, *suffix, ext: str = ".xlsx", sep: str = "_"):
+    
+    def __init__(self, ext: str = ".xlsx", sep: str = "_"):
+        self._ext = ext
+        self._sep = sep
+    
+    def fname(self, name: str, *suffix):
         if not name:
             raise ValueError("The name of the file is empty.")
         a_join = (name,) + suffix
-        fn = sep.join(a_join) + ext
+        fn = self._sep.join(a_join) + self._ext
         return fn
