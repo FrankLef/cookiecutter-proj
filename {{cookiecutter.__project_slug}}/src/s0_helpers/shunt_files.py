09c815eed508d2f5dfc9bc6a45876cb917a8dffe
FrankLef <flefebvre01@hotmail.com>
2024-12-22 10:28:04 -0500

    fix(shunt): add tdict

--- {{cookiecutter.__project_slug}}/src/s0_helpers/shunt_files.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/shunt_files.py
@@ -9,8 +9,14 @@ from pathlib import Path
 from config import settings
 
 from src.s0_helpers import path_finder as pfr
+from src.s0_helpers import tdict as tdict_cls
+
 
 data_paths = settings.data_paths
 data_path = Path(__file__).parents[2].joinpath("data")
 
+# the PathFinder object used everywhere in the module to find the paths
 pathfindr = pfr.PathFinder(paths=data_paths, base_path=data_path)
+
+tdict_path = data_path.joinpath(settings.tdict)
+tdict = tdict_cls.TDict(tdict_path)
