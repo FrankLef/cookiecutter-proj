1188e080691a5c84d6e2a6cbda5e2d372f4ad86b
FrankLef <flefebvre01@hotmail.com>
2024-12-22 10:25:56 -0500

    feat(tdict): create class

--- {{cookiecutter.__project_slug}}/src/s0_helpers/tdict.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/tdict.py
@@ -1,22 +1,17 @@
 from pathlib import Path
 import pandas as pd
 
-from config import settings
-
-data_path = Path(__file__).parents[2].joinpath("data", settings.tdict)
-
-def main(path: Path = data_path, role_rgx: str = ".", process_rgx: str = ".") -> pd.DataFrame:
-    if path.is_file():
-        df = pd.read_excel(path)
-        nrow = len(df.axes[0])
-        msg = f"'{path}' has {nrow} rows."
-        print(msg)
-    else:
-        msg = f"'{path}' not found."
-        raise FileNotFoundError(msg)
-    df = df[df.role.str.match(role_rgx) & df.process.str.match(process_rgx)]
-    if not df.empty:
-        msg = f"No tdict for {role_rgx=} and {process_rgx=}."
-        raise ValueError(msg)
-    # print(df.shape)
-    return df
+class TDict:
+    def __init__(self, path: Path):
+        if not path.is_file():
+            raise FileNotFoundError(f"{path} not found.")
+        self._path = path
+        self._specs = pd.read_excel(path)
+        
+    def get_specs(self, role_rgx: str = ".", process_rgx: str = "."):
+        sel = self._specs.role.str.match(role_rgx) & self._specs.process.str.match(process_rgx)
+        df = self._specs[sel]
+        if df.empty:
+            msg = f"No tdict for {role_rgx=} and {process_rgx=}."
+            raise ValueError(msg)
+        return df
--- {{cookiecutter.__project_slug}}/tests/test_tdict.py
+++ {{cookiecutter.__project_slug}}/tests/test_tdict.py
@@ -1,11 +1,23 @@
 import pytest
-import src.s0_helpers.tdict as tdict
+from pathlib import Path
+from src.s0_helpers import tdict as tdict_cls
+
+from config import settings
+
+@pytest.fixture
+def tdict_path():
+    a_path = Path(__file__).parents[1].joinpath("data")
+    a_path = a_path.joinpath(settings.tdict)
+    return a_path
 
 
-def test_tdict():
-    df = tdict.main()
-    assert not df.empty
+def test_tdict(tdict_path):
+    tdict = tdict_cls.TDict(tdict_path)
+    specs = tdict.get_specs()
+    assert not specs.empty
 
-def test_tdict_err():
+
+def test_tdict_err(tdict_path):
+    tdict = tdict_cls.TDict(tdict_path)
     with pytest.raises(ValueError):
-        tdict.main(role_rgx="wrong")
+        tdict.get_specs(role_rgx="wrong")
