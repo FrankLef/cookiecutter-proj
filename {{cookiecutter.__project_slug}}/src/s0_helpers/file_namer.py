4dc73f4a576a5d0c6f92d0436baf43aaf0f9f140
FrankLef <flefebvre01@hotmail.com>
2024-12-22 14:59:42 -0500

    docs(file namer): docs

--- {{cookiecutter.__project_slug}}/src/s0_helpers/file_namer.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/file_namer.py
@@ -1,10 +1,29 @@
 class FileNamer:
+    """Write a file name to a string.
+    """
     
     def __init__(self, ext: str = ".xlsx", sep: str = "_"):
+        """Create a file namer.
+
+        Args:
+            ext (str, optional): File extension, mustinclude the dot. Defaults to ".xlsx".
+            sep (str, optional): Separator. Defaults to "_".
+        """
         self._ext = ext
         self._sep = sep
     
-    def fname(self, name: str, *suffix):
+    def fname(self, name: str, *suffix) -> str:
+        """Create a file name.
+
+        Args:
+            name (str): Base name for the file.
+
+        Raises:
+            ValueError: The base file name is empty.
+
+        Returns:
+            str: File name.
+        """
         if not name:
             raise ValueError("The name of the file is empty.")
         a_join = (name,) + suffix
