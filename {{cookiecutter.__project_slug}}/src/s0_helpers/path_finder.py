285a2b543fde9e031ded7a8648f551ac3185ca71
FrankLef <flefebvre01@hotmail.com>
2024-12-22 15:39:17 -0500

    docs(pathfinder)

--- {{cookiecutter.__project_slug}}/src/s0_helpers/path_finder.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/path_finder.py
@@ -1,14 +1,39 @@
 from pathlib import Path
 
+
 class PathFinder:
-    
+    """Tools to retrieve path from a dictionary of paths.
+    """
     def __init__(self, paths: dict[str, str], base_path: Path):
+        """Create a path finder with a dictionary and a base path.
+
+        Args:
+            paths (dict[str, str]): Dictionary of paths.
+            base_path (Path): Base path of the paths in `paths`.
+
+        Raises:
+            NotADirectoryError: The output path is not a valid directory.
+        """
         if not base_path.is_dir():
             raise NotADirectoryError("{base_path} not found.")
         self._paths = paths
         self._base_path = base_path
-        
-    def get_path(self, id: str, sub: str | None = None, name: str | None = None) :
+
+    def get_path(self, id: str, sub: str | None = None, name: str | None = None):
+        """Build the path.
+
+        Args:
+            id (str): The path's key in the path dictionary.
+            sub (str | None, optional): The sub-direcotry to join. Defaults to None.
+            name (str | None, optional): Name of the file to add to the path. Defaults to None.
+
+        Raises:
+            KeyError: The `id` is not found in the path dictionary.
+            NotADirectoryError: The output path is invalid.
+
+        Returns:
+            _type_: A complete path.
+        """
         if id in self._paths.keys():
             a_path = self._base_path.joinpath(self._paths[id])
         else:
