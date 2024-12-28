fedcfaffdb4d0c414339eed6a371d3cb78f8a05f
FrankLef <flefebvre01@hotmail.com>
2024-12-28 15:57:45 -0500

    test

--- {{cookiecutter.__project_slug}}/src/s2_transf/transf_test.py
+++
@@ -1,6 +0,0 @@
-from src.s0_helpers.richtools import print_msg
-
-
-def main(subproc: str) -> int:
-    print_msg(" ".join([__name__, subproc]), type="msg")
-    return 0
--- {{cookiecutter.__project_slug}}/tests/test_tdict.py
+++ {{cookiecutter.__project_slug}}/tests/test_tdict.py
@@ -9,7 +9,10 @@ from config import settings
 @pytest.fixture
 def tdict():
     a_path = Path(__file__).parents[1].joinpath("data")
-    a_path = a_path.joinpath(settings.tdict_etl)
+    a_path = a_path.joinpath(settings.tdict)
+    if not a_path.exists():
+        msg = f"The TDict file was not found.\n{a_path}"
+        raise FileNotFoundError(msg)
     data = pd.read_excel(a_path)
     obj = tdict_cls.TDict(data)
     return obj
