0d7f73a999539738a0ddf7245540d3e2d013c978
FrankLef <flefebvre01@hotmail.com>
2024-12-28 16:23:20 -0500

    test

--- {{cookiecutter.__project_slug}}/tests/test_PathFinder.py
+++ {{cookiecutter.__project_slug}}/tests/test_PathFinder.py
@@ -9,19 +9,25 @@ from config import settings
 def data_paths():
     return settings.data_paths
 
-
 @pytest.fixture
 def data_path():
     a_path = Path(__file__).parents[1].joinpath("data")
+    if not a_path.exists():
+        msg = f"The Data directory not found.\n{a_path}"
+        raise NotADirectoryError(msg)
     return a_path
 
-
+pytest.mark.skipif(
+    condition=(not data_path().exists()),
+    reason="Data path must exists. Check the settings.")
 @pytest.fixture
 def pathfindr(data_paths, data_path):
     return pfr.PathFinder(paths=data_paths, base_path=data_path)
 
 
-# @pytest.mark.skip
+pytest.mark.skipif(
+    condition=(not data_path().exists()),
+    reason="Data path must exists. Check the settings.")
 def test_get_data_path(pathfindr, id="extr"):
     a_path = pathfindr.get_path(id=id)
     target = Path(__file__).parents[1].joinpath("data", "d1_extr")
--- {{cookiecutter.__project_slug}}/tests/test_tdict.py
+++ {{cookiecutter.__project_slug}}/tests/test_tdict.py
@@ -7,9 +7,17 @@ from config import settings
 
 
 @pytest.fixture
-def tdict():
+def data_path():
     a_path = Path(__file__).parents[1].joinpath("data")
-    a_path = a_path.joinpath(settings.tdict)
+    if not a_path.exists():
+        msg = f"The Data directory not found.\n{a_path}"
+        raise NotADirectoryError(msg)
+    return a_path
+
+
+@pytest.fixture
+def tdict(data_path):
+    a_path = data_path.joinpath(settings.tdict)
     if not a_path.exists():
         msg = f"The TDict file was not found.\n{a_path}"
         raise FileNotFoundError(msg)
@@ -17,12 +25,16 @@ def tdict():
     obj = tdict_cls.TDict(data)
     return obj
 
-
+pytest.mark.skipif(
+    condition=(not data_path().exists()),
+    reason="Data path must exists. Check the settings.")
 def test_tdict(tdict):
     specs = tdict.get_data()
     assert not specs.empty
 
-
+pytest.mark.skipif(
+    condition=(not data_path().exists()),
+    reason="Data path must exists. Check the settings.")
 def test_tdict_err(tdict):
     with pytest.raises(UserWarning):
         tdict.get_data(role="wrong")
