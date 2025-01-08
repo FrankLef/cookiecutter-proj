de23fcec46daff09e21a7a14b6a80395a1fee3be
FrankLef <flefebvre01@hotmail.com>
2025-01-08 12:52:07 -0500

    feat(mmult)

--- {{cookiecutter.__project_slug}}/src/s0_helpers/mmult.py
+++ {{cookiecutter.__project_slug}}/src/s0_helpers/mmult.py
@@ -4,9 +4,9 @@ import pandera as pa
 
 class MMult:
     """Matrix Multiplier for Finance and BI."""
-    
+
     _KEYS = ["index", "variable"]
-    
+
     _SCHEMA = pa.DataFrameSchema(
         {
             "index": pa.Column(str),
@@ -22,13 +22,11 @@ class MMult:
         type(self)._SCHEMA.validate(mat_long)
         self._mat_long = mat_long.copy()
         self._to_wide()  # create matrix in wide format
-        
-    
+
     def _to_wide(self):
-        mat_wide = self._mat_long.pivot(index='index', columns='variable')
-        self._mat_wide = mat_wide
-    
-    
+        mat_wide = self._mat_long.pivot(index="index", columns="variable")
+        self._mat_wide = mat_wide.fillna(0)
+
     def mmult(self, data: pd.DataFrame):
         # check lengths
         if len(data.columns) != len(self._mat_wide.index):
@@ -41,11 +39,11 @@ class MMult:
             msg = f"Column and row index must match exactly. {err_nb} invalid indexes."
             raise ValueError(msg)
         # all OK, do the matrix multiplication
-        out = data.dot.self._mat_wide
+        out = data.dot(self._mat_wide)
         self._data = data
         self._out = out
         return out
-    
+
     @property
     def mat_wide(self):
         return self._mat_wide
@@ -53,7 +51,7 @@ class MMult:
     @property
     def data(self):
         return self._data
-    
+
     @property
     def out(self):
         return self._out
