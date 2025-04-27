e9389ae3844a7330b17db32ad2a832e3a5c2fe2c
FrankLef <flefebvre01@hotmail.com>
2025-04-27 09:13:18 -0400

    feat(registry)

--- {{cookiecutter.__project_slug}}/config.py
+++ {{cookiecutter.__project_slug}}/config.py
@@ -1,3 +1,4 @@
+"""Settings configuration used by dynaconf"""
 from dynaconf import Dynaconf  # type: ignore
 from pathlib import Path
 
@@ -12,4 +13,12 @@ settings = Dynaconf(
 project_path = Path(__file__).parent.absolute()
 settings.paths = {
     "data": project_path.joinpath("data"),
+    "data_temp": project_path.joinpath("data", "d0_temp"),
+    "data_extr": project_path.joinpath("data", "d1_extr"),
+    "data_transf": project_path.joinpath("data", "d2_transf"),
+    "data_load": project_path.joinpath("data", "d3_load"),
+    "data_raw": project_path.joinpath("data", "d4_raw"),
+    "data_pproc": project_path.joinpath("data", "d5_pproc"),
+    "data_eda": project_path.joinpath("data", "d6_eda"),
+    "data_final": project_path.joinpath("data", "d7_final"),
 }
---
+++ {{cookiecutter.__project_slug}}/src/registry.py
@@ -0,0 +1,15 @@
+"""Module used to hold constants and shared values."""
+
+import os
+
+def say_hello() -> str:
+    """Example function for the registry.
+
+    Args:
+        name (str, optional): Name to use to say hello. Defaults to "Ephel".
+
+    Returns:
+        str: Hello message.
+    """
+    msg = "[cyan]" + " ".join(["\u2139", "Hello", os.getlogin()]) + "[/cyan]"
+    return msg
