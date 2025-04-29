68f601cc59bd041c1cb1cdc9ad781e96ea1eeaf1
FrankLef <flefebvre01@hotmail.com>
2025-04-29 15:52:13 -0400

    refactor(run_moduls)

--- {{cookiecutter.__project_slug}}/src/__main__.py
+++ {{cookiecutter.__project_slug}}/src/__main__.py
@@ -1,67 +1,33 @@
 """Main CLI entry point."""
 
-from typing import Final
 import typer
-# import importlib
-# import winsound
 
 from .run_moduls import main as run_cmd
-from src.s0_helpers.richtools import print_msg
 
 app = typer.Typer()
 
 
 @app.command()
-def pipe(tasks: str, pat: str | None = None) -> int:
+def pipe(jobs: str, pat: str | None = None) -> int:
     """Run a pipe of commands.
 
-    The `tasks` argument is a comma-separated string with the task ids. Task ids must be one of 'ex', 'tr', 'lo', 'ra', 'pp', 'ed', 'fi'. For example, you can use 'ex,tr' for 'extract' then 'transform'. The order does not matter.
-    The `tasks` is case-insensitive. All spaces are removed. The task id could be any word as long as it starts with the proper two letter. For example 'transform_it, EXT' is the same as 'ex,tr'.  If a task appears more that once, it will be processed only once.
+    The `jobs` argument is a comma-separated string with the job's id. Job ids must be one of 'ex', 'tr', 'lo', 'ra', 'pp', 'ed', 'fi'. For example, you can use 'ex,tr' for 'extract' then 'transform'. The order does not matter.
+    The `jobs` is case-insensitive. All spaces are removed. The task id could be any word as long as it starts with the proper two letter. For example 'transform_it, EXT' is the same as 'ex,tr'.  If a task appears more that once, it will be processed only once.
 
     Args:
-        tasks (str): comma-separated string with the task.
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Raises:
-        KeyError: There are invalid task id.
-        IndexError: There are too many tasks.
-        ValueError: The `tasks` argument is empty.
+        jobs (str): comma-separated string with the jobs.
+        pat (str | None, optional): Regex patttern passed on to the command to fitler files. Defaults to None.
 
     Returns:
-        int: The sum of all the integers returned by the tasks.
+        int: The sum of all the integers returned by the jobs.
     """
-    SEP: Final[str] = ","
-    TASKS: Final[tuple[str, ...]] = ("ex", "tr", "lo", "ra", "pp", "ed", "fi")
-
-    tasks = tasks.replace(" ", "")
-
-    if tasks:
-        tasks_todo = tasks.lower().split(sep=SEP)
-        tasks_todo = [x[:2] for x in tasks_todo]
-        ntasks_todo = len(tasks_todo)
-        if ntasks_todo <= len(TASKS):
-            err_nb = sum([x not in TASKS for x in tasks_todo])
-            if err_nb:
-                msg = f"There are {err_nb} invalid tasks in '{tasks}'.\nThe task first 2 characters must be in {TASKS}."
-                raise KeyError(msg)
-        else:
-            msg = f"There are {ntasks_todo} tasks. There must be no more than {len(TASKS)}."
-            raise IndexError(msg)
-    else:
-        raise ValueError("The `tasks` argument must be provided.")
-
-    print_msg(f"Processing {ntasks_todo} main tasks \u2026", type="process")
-    n: int = 0
-    # NOTE: run the command in the pre-determined order
-    for proc in TASKS:
-        if proc in tasks_todo:
-            n += run_cmd(proc=proc, pat=pat)
+    n = run_cmd(jobs=jobs, pat=pat)
     return n
 
 
 @app.command()
-def extr(pat: str | None = None) -> int:
-    """Extract data from an external source.
+def all(pat: str | None = None) -> int:
+    """Run all modules.
 
     Args:
         pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
@@ -69,91 +35,8 @@ def extr(pat: str | None = None) -> int:
     Returns:
         int: Integer returned by the process.
     """
-    n = run_cmd(proc="extr", pat=pat)
-    return n
-
-
-@app.command()
-def transf(pat: str | None = None) -> int:
-    """Tranform the extracted data to a table format.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="transf", pat=pat)
-    return n
-
-
-@app.command()
-def load(pat: str | None = None) -> int:
-    """Upload to an external database.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="load", pat=pat)
-    return n
-
-
-@app.command()
-def raw(pat: str | None = None) -> int:
-    """Get raw data for EDA.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="raw", pat=pat)
-    return n
-
-
-@app.command()
-def pproc(pat: str | None = None) -> int:
-    """Preprocess data for EDA.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="pproc", pat=pat)
-    return n
-
-
-@app.command()
-def eda(pat: str | None = None) -> int:
-    """Exploratory Data Analysis.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="eda", pat=pat)
-    return n
-
-
-@app.command()
-def final(pat: str | None = None) -> int:
-    """Finalize EDA.
-
-    Args:
-        pat (str | None, optional): Patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: Integer returned by the process.
-    """
-    n = run_cmd(proc="final", pat=pat)
+    jobs = "ex,tr,lo,ra,pp,ed,fi"
+    n = run_cmd(jobs=jobs, pat=pat)
     return n
 
 
--- {{cookiecutter.__project_slug}}/src/run_moduls.py
+++ {{cookiecutter.__project_slug}}/src/run_moduls.py
@@ -2,13 +2,14 @@
 
 from importlib import import_module
 from pathlib import Path
-from typing import Final
+from typing import Final, Any
+import re
 import winsound
 
-from src.s0_helpers.richtools import print_modul
+from src.s0_helpers.richtools import print_msg, print_modul
 
 
-def play_note(song: str, duration: int = 500, wake: bool = True) -> None:
+def play_note(song: str, duration: int = 500, wake: bool = True) -> str:
     """Play a sequence of notes."""
     assert len(song) != 0, "The song must have at least one note."
     notes = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11}
@@ -19,14 +20,17 @@ def play_note(song: str, duration: int = 500, wake: bool = True) -> None:
     for note in song.upper().split():
         freq = int(256 * (2 ** (notes[note] / 12)))
         winsound.Beep(frequency=freq, duration=duration)
+    return song
 
 
-def run_proc(dir: str, pat: str) -> int:
+def run_modul(dir: str, pat: str) -> int:
     """Process the modules in the src directory with given pattern."""
     wd = Path(__file__).parent.joinpath(dir)
-    if not wd.exists():
+    if wd.exists():
+        files = [item for item in wd.iterdir() if item.is_file()]
+    else:
         raise NotADirectoryError(f"Invalid path\n{wd}")
-    names = sorted([f.stem for f in wd.glob(pat) if f.is_file()])
+    names = sorted([fn.stem for fn in files if re.match(pat, fn.name)])
     if not len(names):
         raise ValueError(f"No module found in\n{wd}")
     n: int = 0
@@ -42,16 +46,15 @@ def run_proc(dir: str, pat: str) -> int:
 
 
 def get_pattern(suffix: str, pat: str | None = None) -> str:
-    """Create the pattern for glob()"""
-    stem = suffix + "*_*"
+    """Create the regex pattern used to filter the files."""
     if pat is None:
-        pat = stem + ".py"
+        pat = suffix + r".+_.*" + "[.]py"
     else:
-        pat = stem + pat + ".py"
+        pat = suffix + r".+_" + pat + "[.]py"
     return pat
 
 
-def get_specs(proc: str) -> tuple[str, ...]:
+def get_specs(job: str | None = None) -> Any:
     """Get the procedure's specs."""
     MODULS: Final[dict[str, tuple[str, ...]]] = {
         "ex": ("extr", "s1_extr", "C"),
@@ -62,13 +65,53 @@ def get_specs(proc: str) -> tuple[str, ...]:
         "ed": ("eda", "s6_eda", "A"),
         "fi": ("final", "s7_final", "B"),
     }
-    specs = MODULS[proc]
-    return specs
+    if job is not None:
+        out = MODULS[job]  # type: ignore
+    else:
+        out = MODULS  # type: ignore
+    return out
 
 
-def main(proc: str, pat: str | None = None) -> int:
-    specs = get_specs(proc=proc)
+def run_job(job: str, pat: str | None = None) -> int:
+    """Execute a single job."""
+    specs = get_specs(job=job)
     pat = get_pattern(suffix=specs[0], pat=pat)
-    n = run_proc(dir=specs[1], pat=pat)
+    n = run_modul(dir=specs[1], pat=pat)
     play_note(song=specs[2])
+    return n
+
+
+def get_jobs(jobs: str) -> list[str]:
+    """Get the oredered list of jobs."""
+    jobs_list = get_specs().keys()
+
+    # remove all whitspace, tab, newline, etc
+    jobs = re.sub(r"\s+", "", jobs)
+
+    if jobs:
+        jobs_todo = jobs.lower().split(sep=",")
+        jobs_todo = [x[:2] for x in jobs_todo]
+        njobs_todo = len(jobs_todo)
+        if njobs_todo <= len(jobs_list):
+            err_nb = sum([x not in jobs_list for x in jobs_todo])
+            if err_nb:
+                msg = f"There are {err_nb} invalid jobs in '{jobs_todo}'.\nThe job's first 2 characters must be in {jobs_list}."
+                raise KeyError(msg)
+        else:
+            msg = f"There are {jobs_todo} jobs to do. There must be no more than {len(jobs_list)}."
+            raise IndexError(msg)
+    else:
+        raise ValueError("The `jobs` are empty.")
+    assert len(jobs_todo) != 0, "No jobs. This should not reach this point!"
+    # reorder the list to match the fixed jobs list
+    jobs_todo = [x for x in jobs_list if x in jobs_todo]
+    return jobs_todo
+
+
+def main(jobs: str, pat: str | None = None) -> int:
+    jobs_todo = get_jobs(jobs=jobs)
+    print_msg(f"Processing {len(jobs)} jobs_todo \u2026", type="process")
+    n: int = 0
+    for job in jobs_todo:
+        n += run_job(job=job, pat=pat)
     return n
