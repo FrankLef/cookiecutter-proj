6bdc2681ad2c5b2ffcc391d5547ac274894df5db
FrankLef <flefebvre01@hotmail.com>
2025-10-20 18:04:38 -0400

    feat(final): export

--- {{cookiecutter.__project_slug}}/src/s7_final/Xfinal08a_xprt.py
+++ {{cookiecutter.__project_slug}}/src/s7_final/Xfinal08a_xprt.py
@@ -1,58 +1,97 @@
 """Export files to reports."""
 
-import warnings
 import shutil
 from rich import print as rprint
+from typing import NamedTuple
+from pathlib import Path
 
 from config import settings
 
 path_transf = settings.paths.data_transf
 path_raw = settings.paths.data_raw
-path_reports = settings.paths.data_reports
+path_pproc = settings.paths.data_pproc
+path_eda = settings.paths.data_eda
+path_reports = settings.paths.reports_data
 
+class XprtFile(NamedTuple):
+    name: str
+    skip: bool
 
-def get_xprt():
-    """Define the dictionary of files to export."""
-    out = {
-        "transf": {"path": path_transf, "files": ("dendo.png", "hclus_var.html")},
-        "raw": {
-            "path": path_raw,
-            "files": (
-                "clus_dist_A.html",
-                "clus_dist_B.html",
-                "clus_dist_C.html",
-                "clus_dist_D.html",
-            ),
-        },
-    }
-    return out
+class XprtDir(NamedTuple):
+    name: str
+    path: Path
+    files: list[XprtFile]
+    
+xprt_transf = XprtDir(
+    name="transf",
+    path=path_transf,
+    files=[
+        XprtFile(name="clus_client_3D.html", skip=False),
+        XprtFile(name="clus_client_bubbles.html", skip=False),
+        XprtFile(name="clus_genus_3D.html", skip=False),
+        XprtFile(name="clus_genus_bubbles.html", skip=False),
+    ]
+)
 
+xprt_raw = XprtDir(
+    name="raw",
+    path=path_raw,
+    files=[
+        XprtFile(name="catfr_crosstabl.html", skip=False),
+        XprtFile(name="clus_crosstabl.html", skip=False),
+        XprtFile(name="violin_catfr.html", skip=False),
+        XprtFile(name="violin_genus.html", skip=False),
+    ]
+)
 
-def main(is_skipped: bool = True) -> int:
-    """Main function.
+xprt_pproc = XprtDir(
+    name="pproc",
+    path=path_pproc,
+    files=[
+        XprtFile(name="mba_tabl.html", skip=False),
+    ]
+)
 
-    Args:
-        is_skipped (bool, optional): Skip this module if True. Defaults to False.
+xprt_eda = XprtDir(
+    name="eda",
+    path=path_eda,
+    files=[
+        XprtFile(name="elast_catfr.html", skip=False),
+        XprtFile(name="elast_genus.html", skip=False),
+    ]
+)
 
-    Returns:
-        int: Return an integer on the status.
-    """
-    if is_skipped:
-        warnings.warn(f"{__name__} is skipped.", category=UserWarning)
-        return 0
-    xprt = get_xprt()
-    if not len(xprt):
-        raise ValueError("No file to export.")
-    n: int = 0
-    for key, val in xprt.items():
-        src_path = val["path"]
-        files = val["files"]
-        msg = f"'{key}': Exporting {len(files)} files from\n{src_path}\nto\n{path_reports}"
-        rprint(msg)
-        for fn in files:
-            src_fn = src_path.joinpath(fn)
-            dest_fn = path_reports.joinpath(fn)
-            # use copy2() to keep the metadata (e.g. date)
+xprts = [xprt_transf, xprt_raw, xprt_pproc, xprt_eda]
+
+def export_files(xprt: XprtDir, path: Path)-> int:
+    nfiles :int = 0
+    for a_file in xprt.files:
+        if not a_file.skip:
+            msg = f"""
+            Exporting '{a_file.name}'
+            from {xprt.path}
+            to
+            {path}
+            """
+            rprint(msg)
+            src_fn = xprt.path.joinpath(a_file.name)
+            dest_fn = path.joinpath(a_file.name)
             shutil.copy2(src=src_fn, dst=dest_fn)
-            n += 1
-    return n
+            nfiles += 1
+    if not nfiles:
+        msg = f"No file exported from '{xprt.name}'."
+        rprint(msg)
+    return nfiles
+
+
+def main(is_skipped: bool = True) -> None:
+    if is_skipped:
+        raise NotImplementedError(f"Skip the '{__name__}' script.")
+    
+    if len(xprts):
+        for xprt in xprts:
+            nfiles = export_files(xprt, path=path_reports)
+            msg = f"{nfiles} files exported from '{xprt.name}'."
+            rprint(msg)
+    else:
+        raise ValueError("No file to export.")
