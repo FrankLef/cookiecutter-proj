7c1ea77903fd5da1250d0bd88641d9c7f55e205e
FrankLef <flefebvre01@hotmail.com>
2026-07-01 08:10:42 -0400

    refactor(registry)

--- {{cookiecutter.__project_slug}}/src/_registry/acc.py
+++ {{cookiecutter.__project_slug}}/src/_registry/acc.py
@@ -1,20 +1,20 @@
 """Instantiate a connection to a MS Access database."""
 
-from pathlib import Path
+# from pathlib import Path
 
-from config import settings
+# from config import settings
 
-from src.s0_helpers import connect_acc as acc
+# from fltk.conn import connect_acc as acc
 
 
-def main(db_choice: str) -> tuple[acc.ConnectAcc, Path]:
-    match db_choice:
-        case "db":
-            path = Path(settings.db.xbr)
-        case "raw":
-            path = Path(settings.raw_db.xbr)
-        case _:
-            msg: str = f"'{db_choice}' is an invalid db choice for accdb_conn."
-            raise ValueError(msg)
-    conn = acc.ConnectAcc(path=path)
-    return (conn, path)
+# def main(db_choice: str) -> tuple[acc.ConnectAcc, Path]:
+#     match db_choice:
+#         case "db":
+#             path = Path(settings.db.xbr)
+#         case "raw":
+#             path = Path(settings.raw_db.xbr)
+#         case _:
+#             msg: str = f"'{db_choice}' is an invalid db choice for accdb_conn."
+#             raise ValueError(msg)
+#     conn = acc.ConnectAcc(path=path)
+#     return (conn, path)
