7d23e422aa527c0e1976cffd734002b5ebdd4ffe
FrankLef <flefebvre01@hotmail.com>
2026-07-11 02:29:08 -0400

    refactor(workflow)

--- {{cookiecutter.__project_slug}}/src/__main__.py
+++ {{cookiecutter.__project_slug}}/src/__main__.py
@@ -16,15 +16,9 @@ workflow = wf.WorkFlow(root=root_path, wf_path=wf_path)
 def pipe(jobs: str, pat: str | None = None) -> None:
     """Run a pipe of commands.
 
-    The `jobs` argument is a comma-separated string with the job's id. Job ids must be one of 'se', 'ex', 'tr', 'lo', 'ra', 'pp', 'ed', 'te'. For example, you can use 'ex,tr' for 'extract' then 'transform'. The order does not matter.
-    The `jobs` is case-insensitive. All spaces are removed. The task id could be any word as long as it starts with the proper two letter. For example 'transform_it, EXT' is the same as 'ex,tr'.  If a task appears more that once, it will be processed only once.
-
     Args:
-        jobs (str): comma-separated string with the jobs.
-        pat (str | None, optional): Regex patttern passed on to the command to fitler files. Defaults to None.
-
-    Returns:
-        int: The sum of all the integers returned by the jobs.
+        jobs (str): Comma-separated string with the jobs.
+        pat (str | None, optional): Regex patttern to fitler files. Defaults to None.
     """
     workflow.execute(jobs_args=jobs, pat=pat)
 
@@ -39,8 +33,14 @@ def all(pat: str | None = None) -> None:
     Returns:
         int: Integer returned by the process.
     """
-    jobs = "se,ex,tr,lo,ra,pp,ed"
+    jobs = "etl,pproc,rollup,survey,outl,eda"
     workflow.execute(jobs_args=jobs, pat=pat)
+
+
+@app.command()
+def upload() -> None:
+    """Upload data to MS Access."""
+    workflow.execute(jobs_args="teard", pat="upload")
 
 
 if __name__ == "__main__":
