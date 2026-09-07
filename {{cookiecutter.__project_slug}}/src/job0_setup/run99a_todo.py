f67cd010670b3d94225f4008e3b3757f9e9efec2
FrankLef <flefebvre01@hotmail.com>
2026-09-07 18:35:18 -0400

    fix(scriptrun)

--- {{cookiecutter.__project_slug}}/src/job0_setup/run99a_todo.py
+++ {{cookiecutter.__project_slug}}/src/job0_setup/run99a_todo.py
@@ -1,19 +1,8 @@
-"""A job template, used as a placeholder."""
-
+"""A run template, used as a placeholder."""
 from rich import print as rprint
 
 
-def main(is_skipped: bool = False) -> None:
-    """Main function.
-
-    Args:
-        is_skipped (bool, optional): Skip this module if True. Defaults to False.
-
-    Returns:
-        int: Return an integer on the status.
-    """
-    if is_skipped:
-        raise NotImplementedError(f"Skip the '{__name__}' script.")
+def main() -> None:
     rprint(f"Executing the '{__name__}' script.")
 
 
